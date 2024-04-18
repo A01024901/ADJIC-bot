eedea0ee36ab9a335eff8b01537de2e298a1aed6
A01024901 <A01024901@itesm.mx>
2024-04-18 13:12:49 -0600

    Nodo joints

--- Minichallenges/minichal1/src/tf2_broadcaster.py
+++ Minichallenges/minichal1/src/tf2_broadcaster.py
@@ -3,6 +3,8 @@
 import rospy 
 import tf2_ros
 import numpy as np
+from std_msgs.msg import Float32
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
 from tf.transformations import quaternion_from_euler
 
@@ -12,74 +14,120 @@ class tf_broadcaster:
         rospy.init_node('tf2_broadcaster')
         rospy.on_shutdown(self.cleanup)
 
-        ###--- Subscriptores ---###
-        #rospy.Subscriber("/cmd_vel" , Twist , self.vel_cb)
+        ###--- Subscribers ---###
+        rospy.Subscriber("wr" , Float32 , self.wr_cb)
+        rospy.Subscriber("wl" , Float32 , self.wl_cb)
+        rospy.Subscriber("odom" , Odometry , self.odom_cb)
 
         ###--- Broadcaster ---###
         self.tf_broadcast = tf2_ros.TransformBroadcaster()
 
-        ###--- Transformation ---###
-        self.t = TransformStamped()
+        ###--- Transformations ---###
+        self.right_wheel = TransformStamped()
+        self.left_wheel = TransformStamped()
+        self.puzzlebot = TransformStamped()
+
+        ###--- Variables Puzzlebot ---###
+        self.x = 0
+        self.y = 0
+        self.orientation_x = 0
+        self.orientation_y = 0
+        self.orientation_z = 0
+        self.orientation_w = 0
+
+        ###--- Variables Ruedas ---###
+        self.theta_r = 0
+        self.theta_l = 0
+        self.wr = 0
+        self.wl = 0
 
         ###--- Robot Pose ---###
-        self.x = 1.0
-        self.y = 0.0
-        self.theta = 0.0
-        
-        rate = rospy.Rate(50)
-
-        #while rospy.get_time() == 0: print ("Simulacion no iniciada")#Descomentar en simulacion 
+        rate = rospy.Rate(30)
 
         print("Broadcast operando")
         print("Use rviz to see rotation")
 
         while not rospy.is_shutdown():
-            
-            # Fill the transformation information 
 
-            self.t.header.stamp = rospy.Time.now() 
-
-            self.t.header.frame_id = "odom" 
-
-            self.t.child_frame_id = "base_link" 
-
-            self.t.transform.translation.x = self.x 
-
-            self.t.transform.translation.y = self.y 
-
-            self.t.transform.translation.z = 0.0 
-
-            self.theta += 0.01 #theta will be increasing 
-
-            #Clip the value of theta to the interval [-pi to pi] 
-
-            theta = np.arctan2(np.sin(self.theta), np.cos(self.theta)) 
-
-            #The transformation requires the orientation as a quaternion 
-
-            q = quaternion_from_euler(0, 0, self.theta) 
-
-            self.t.transform.rotation.x = q[0] 
-
-            self.t.transform.rotation.y = q[1] 
-
-            self.t.transform.rotation.z = q[2] 
-
-            self.t.transform.rotation.w = q[3] 
-
-            # A transformation is broadcasted instead of published 
-
-            self.tf_broadcast.sendTransform(self.t) #broadcast the transformation 
+            self.move_puzzlebot('Origin' , 'Bot')
+            self.rotate_wheel_r('Bot' , 'right_w')
+            self.rotate_wheel_l('Bot' , 'left_w')
 
             rate.sleep()
 
-    def rotate_wheel(self):
-        pass
+    def rotate_wheel_r(self , parent_frame , wheel_frame):
+        self.right_wheel.header.stamp = rospy.Time.now() 
+        self.right_wheel.header.frame_id = parent_frame
+        self.right_wheel.child_frame_id = wheel_frame
 
+        self.right_wheel.transform.translation.x = -1
+        self.right_wheel.transform.translation.y = 1
+
+        self.theta_r += self.wr #theta will be increasing 
+
+        #Clip the value of theta to the interval [-pi to pi] 
+        self.theta_r = np.arctan2(np.sin(self.theta_r), np.cos(self.theta_r)) 
+
+        #The transformation requires the orientation as a quaternion 
+        q = quaternion_from_euler(0, 0, self.theta_r) 
+        self.right_wheel.transform.rotation.x = q[0]
+        self.right_wheel.transform.rotation.y = q[1]
+        self.right_wheel.transform.rotation.z = q[2]
+        self.right_wheel.transform.rotation.w = q[3]
+
+        self.tf_broadcast.sendTransform(self.right_wheel) #broadcast the transformation 
+
+    def rotate_wheel_l(self , parent_frame , wheel_frame):
+        self.left_wheel.header.stamp = rospy.Time.now() 
+        self.left_wheel.header.frame_id = parent_frame
+        self.left_wheel.child_frame_id = wheel_frame
+
+        self.left_wheel.transform.translation.x = 1
+        self.left_wheel.transform.translation.y = 1
+
+        self.theta_l += self.wl #theta will be increasing 
+
+        #Clip the value of theta to the interval [-pi to pi] 
+        self.theta_l = np.arctan2(np.sin(self.theta_l), np.cos(self.theta_l)) 
+
+        #The transformation requires the orientation as a quaternion 
+        q = quaternion_from_euler(0, 0, self.theta_l) 
+        self.left_wheel.transform.rotation.x = q[0]
+        self.left_wheel.transform.rotation.y = q[1]
+        self.left_wheel.transform.rotation.z = q[2]
+        self.left_wheel.transform.rotation.w = q[3]
+
+        self.tf_broadcast.sendTransform(self.left_wheel) #broadcast the transformation 
+
+
+    def move_puzzlebot(self , parent_frame , bot_frame):
+        self.puzzlebot.header.stamp = rospy.Time.now() 
+        self.puzzlebot.header.frame_id = parent_frame
+        self.puzzlebot.child_frame_id = bot_frame
+        #The transformation requires the orientation as a quaternion 
+        self.puzzlebot.transform.rotation.x = self.orientation_x
+        self.puzzlebot.transform.rotation.y = self.orientation_y
+        self.puzzlebot.transform.rotation.z = self.orientation_z
+        self.puzzlebot.transform.rotation.w = self.orientation_w
+
+        self.tf_broadcast.sendTransform(self.puzzlebot) #broadcast the transformation 
+
+    def wr_cb(self , msg):
+        self.wr = msg.data
+
+    def wl_cb(self , msg):
+        self.wl = msg.data
+
+    def odom_cb (self , msg):
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        self.orientation_x = msg.pose.pose.orientation.x
+        self.orientation_y = msg.pose.pose.orientation.y
+        self.orientation_z = msg.pose.pose.orientation.z
+        self.orientation_w = msg.pose.pose.orientation.w
 
     def cleanup (self):
-        print ("Esta transmision se ha terminado")
-        
+        print ("Esta transmision se ha terminado")    
 
 if __name__ == "__main__": 
     tf_broadcaster()
