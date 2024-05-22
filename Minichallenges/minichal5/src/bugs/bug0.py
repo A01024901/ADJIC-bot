48c1376821c90616b47d939b90f1cd04b1483fe4
A01024901 <A01024901@itesm.mx>
2024-05-21 18:55:04 -0600

    Organice repo

---
+++ Minichallenges/minichal5/src/bugs/bug0.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3 
+
+import rospy 
+import numpy as np
+from std_msgs.msg import Bool
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseStamped
+import tf.transformations as tf
+from geometry_msgs.msg import Twist
+
+class follow_walls:
+    def __init__(self):
+        ###--- Inicio del Nodo ---###
+        rospy.init_node('follow_wall')
+        rospy.on_shutdown(self.cleanup)
+
+        ###--- Subscriptores ---###
+        rospy.Subscriber("/puzzlebot_1/scan", LaserScan, self.laser_cb)
+        rospy.Subscriber("pos_gtg" , PoseStamped , self.pos_gtg_cb) 
+        rospy.Subscriber("target_gtg" , PoseStamped , self.target_gtg_cb)
+
+        ###--- Publishers ---###
+        self.pub_cmd_vel = rospy.Publisher('fw_twist', Twist, queue_size=1) 
+        self.pub_flag_front = rospy.Publisher('front_object', Bool, queue_size=1) 
+        self.pub_flag_clear = rospy.Publisher('front_clear', Bool, queue_size=1) 
+
+        ###--- Constants ---###
+        self.follow_distance = 0.25
+        self.safe_distance = 0.07
+        self.dt = 0.02
+        self.k_w_AO = 0.4
+        self.k_w_C_AO = 2.8
+        self.v_C_AO = 0.15
+
+        ###--- Objetos ---###
+        self.cmd_vel = Twist() 
+        self.scan = LaserScan()
+        self.object_flag = Bool()
+        self.clear_flag = Bool()
+        rate = rospy.Rate(int(1.0/self.dt))
+
+        ###--- Variables ---###
+        self.scan_flag = False
+        self.crash_state = False
+        self.distance_temp = 0
+        self.x_pos = 0
+        self.y_pos = 0
+        self.x_goal = 0
+        self.y_goal = 0
+        self.th_goal = 0
+
+        while rospy.get_time() == 0: pass 
+
+        while not rospy.is_shutdown():
+            if self.scan.ranges:
+                d = np.sqrt(((self.x_goal - self.x_pos) ** 2) + ((self.y_goal - self.y_pos) ** 2))
+                self.scan_flag = False
+                closest_distance = min(self.scan.ranges)
+                index = self.scan.ranges.index(closest_distance)
+                closest_angle = self.scan.angle_min + index * self.scan.angle_increment
+                closest_angle = np.arctan2(np.sin(closest_angle), np.cos(closest_angle))  
+                self.eval_conditions(closest_distance , closest_angle)
+                
+                self.object_flag.data = self.crash_state
+
+                self.th_goal = np.arctan2((self.y_goal - self.x_pos), (self.x_goal - self.y_pos))
+
+                rot = self.eval_rotation(closest_distance , closest_angle)
+
+                state = self.safe_zone(closest_distance , closest_angle)
+                print (closest_distance)
+
+                if self.safe_zone(closest_distance , closest_angle):
+                    print("Safe_Zone")
+                    self.cmd_vel.linear.x = 0.0
+                    self.cmd_vel.angular.z = 0.25 * rot
+                
+                else: self.calc_fw(rot , closest_angle)
+                
+            ###--- Publish ---###
+            self.pub_cmd_vel.publish(self.cmd_vel)
+            self.pub_flag_front.publish(self.object_flag)
+            self.pub_flag_clear.publish(self.clear_flag)
+            rate.sleep()
+
+    def eval_conditions(self , distance , angle):
+        distance = distance <= self.follow_distance
+        angle = angle > -np.pi/4 and angle < np.pi/4
+        if distance and angle: 
+            self.crash_state = True
+
+    def safe_zone(self , distance , angle):
+        distance = distance <= self.safe_distance
+        angle = angle > -np.pi/4 and angle < np.pi/4
+        return distance
+
+    def eval_rotation(self , cl_distance , cl_angle):
+        fw_th = (np.pi / 2) + cl_angle
+        fw_th = np.arctan2(np.sin(fw_th), np.cos(fw_th))  
+        if (cl_distance < self.follow_distance) and (abs(fw_th - self.th_goal) <= (np.pi / 2.0) - 0.2) :
+            return True # = Counter Clock Wise
+        elif (cl_distance < self.follow_distance) and (abs(fw_th - self.th_goal) > (np.pi / 2.0) - 0.2) :
+            return True # = Counter Clock Wise
+        
+    def calc_fw(self , cw_b , closest_angle):
+        #print ("Follow W")
+        fw_th = (np.pi / 2) + closest_angle if cw_b else -(np.pi / 2) + closest_angle
+        if (-0.08 > fw_th > 0.08) and self.crash_state :
+            v_AO = 0.0
+            w_AO = self.k_w_AO * fw_th
+        else :
+            v_AO = self.v_C_AO
+            w_AO = self.k_w_C_AO * fw_th
+
+        self.cmd_vel.linear.x = v_AO
+        self.cmd_vel.angular.z = w_AO
+        print ("Calc_vel: " ,v_AO , w_AO)
+
+    def clear_path(self):
+        angle_to_goal = np.arctan2((self.y_goal - self.y_pos), (self.x_goal - self.x_pos)) - self.yaw
+        
+        # Normalizar el ángulo
+        angle_to_goal = np.arctan2(np.sin(angle_to_goal), np.cos(angle_to_goal))
+        
+        # Calcular el índice del LIDAR correspondiente al ángulo hacia el objetivo
+        index_goal_angle = int((angle_to_goal - self.scan.angle_min) / self.scan.angle_increment)
+        
+        # Verificar si el índice está dentro del rango del LIDAR
+        if 0 <= index_goal_angle < len(self.scan.ranges):
+            # Verificar si la distancia medida en esa dirección es mayor que la distancia al objetivo
+            distance_to_goal = np.sqrt((self.x_goal - self.x_pos)**2 + (self.y_goal - self.y_pos)**2)
+            if self.scan.ranges[index_goal_angle] > distance_to_goal:
+                self.clear_flag.data = True
+        self.clear_flag.data = False
+
+    def wl_cb (self , msg):
+        self.wl = msg.data
+
+    def pos_gtg_cb(self , msg):
+        self.x_pos = msg.pose.position.x 
+        self.y_pos = msg.pose.position.y
+        orientation_q = msg.pose.orientation
+        _, _, self.yaw = tf.transformations.euler_from_quaternion([orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w])
+    
+    def target_gtg_cb(self , msg):
+        self.x_goal = msg.pose.position.x 
+        self.y_goal = msg.pose.position.y
+
+    def laser_cb (self , msg):
+        self.scan = msg
+        data = msg.ranges
+        #self.scan.ranges = np.roll(data, int(len(data)/2 + 1))   
+
+    def cleanup (self):
+        print ("Apagando Localsation")
+        self.pub_cmd_vel.publish(Twist())
+        self.pub_flag_front.publish(Bool())        
+        
+if __name__ == "__main__": 
+    follow_walls()
---
+++ Minichallenges/minichal5/src/go_to_goal/go_to_goal.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python 
+import rospy  
+import numpy as np 
+import tf.transformations as tf
+from geometry_msgs.msg import Twist 
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Bool
+from geometry_msgs.msg import PoseStamped
+from tf.transformations import quaternion_from_euler
+
+class GoToGoal:  
+    def __init__(self):  
+        ###--- Inicio del Nodo ---###
+        rospy.init_node('go_to_goal')
+        rospy.on_shutdown(self.cleanup)
+
+        ###--- Subscriptores ---###
+        rospy.Subscriber("odom", Odometry, self.odom_cb)  
+        rospy.Subscriber("front_clear", Bool, self.front_clear_cb)
+
+        ###--- Publishers ---###
+        self.pub_cmd_vel = rospy.Publisher('gtg_twist', Twist, queue_size=1)  
+        self.pos_pub = rospy.Publisher('pos_gtg', PoseStamped, queue_size=1)
+        self.pos_t_pub = rospy.Publisher('target_gtg', PoseStamped, queue_size=1)
+        self.at_goal_flag_pub = rospy.Publisher('at_goal_flag', Bool, queue_size=1)
+
+        ###--- Constants ---###
+        self.dt = 0.02
+        map = int(rospy.get_param('world_number' , "1"))
+        print (map)
+        positions = [[0.75 , -0.5] , [0.5 , -3.25] , [4.5,-2.25] , [4.5,-2.25]]
+        positions = [[1.0 , -0.5] , [0.05 , -3.4] , [4.75,-2.25] , [4.75,-2.25]]
+        self.x_target = positions[map][0]
+        self.y_target = positions[map][1]
+
+        ###--- Objetos ---###
+        self.flag_msg = Bool()
+        self.pose = PoseStamped()
+        self.pose_target = PoseStamped()
+        self.v_msg = Twist()
+        rate = rospy.Rate(int(1.0/self.dt))
+
+        ###--- Variables ---###
+        self.state = "go_to_goal"
+        self.at_goal_flag = False
+        self.x_pos = 0.0 
+        self.y_pos = 0.0
+        self.theta_robot = 0.0
+        self.clear_path = False
+
+        while rospy.get_time() == 0: pass #Descomentar en simulacion 
+
+        self.t_ant = rospy.get_time() #Valor para obtener dt 
+
+        while not rospy.is_shutdown(): 
+            if self.state == "go_to_goal":
+                v, w = self.calc_gtg()
+                self.at_goal()
+                self.fill_messages(v, w)
+            elif self.state == "follow_walls":
+                v, w = self.follow_walls_behave()
+
+            self.check_state_transition()
+
+            self.pub_cmd_vel.publish(self.v_msg) 
+            self.pos_pub.publish(self.pose)
+            self.pos_t_pub.publish(self.pose_target)
+            self.at_goal_flag_pub.publish(self.flag_msg)
+            rate.sleep() 
+
+    def calc_gtg (self):
+        kvmax = 0.17
+        kwmax = 0.8
+        av = 2.0
+        aw = 2.0
+        ed = np.sqrt((self.x_target - self.x_pos)**2+(self.y_target - self.y_pos)**2)
+        theta_target = np.arctan2(self.y_target - self.y_pos , self.x_target - self.x_pos)
+        e_theta = theta_target - self.theta_robot
+        e_theta = np.arctan2(np.sin(e_theta), np.cos(e_theta))
+
+        if e_theta != 0:
+            kw = kwmax * (1 - np.exp(-aw * e_theta **2))/abs(e_theta)
+        else:
+            kw = 0.05
+        w = kw * e_theta
+        if abs(e_theta) > np.pi/8:
+            v = 0
+        else:
+            kv = kvmax * (1 - np.exp(-av * ed **2))/abs(ed)
+            v = kv * ed
+
+        return v, w
+    
+    def follow_walls_behave(self):
+        # Aquí implementa la lógica para seguir las paredes
+        v = 0.0
+        w = 0.0
+        return v, w
+
+    def check_state_transition(self):
+        if self.state == "go_to_goal" and self.at_goal_flag:
+            self.state = "follow_walls"
+        elif self.state == "follow_walls" and self.clear_path:
+            self.state = "go_to_goal"
+
+    def at_goal(self):
+        tolerance = 0.1
+        x_window = self.x_target - tolerance < self.x_pos < self.x_target + tolerance
+        y_window = self.y_target - tolerance < self.y_pos < self.y_target + tolerance 
+        self.at_goal_flag = x_window and y_window
+
+    def fill_messages(self, v, w):
+        self.v_msg.linear.x = v
+        self.v_msg.angular.z = w
+        self.pose.pose.position.x = self.x_pos
+        self.pose.pose.position.y = self.y_pos
+        self.pose_target.pose.position.x = self.x_target
+        self.pose_target.pose.position.y = self.y_target
+
+        quat = quaternion_from_euler(0 , 0 , self.theta_robot)
+        self.pose.pose.orientation.x = quat[0]
+        self.pose.pose.orientation.y = quat[1]
+        self.pose.pose.orientation.z = quat[2]
+        self.pose.pose.orientation.w = quat[3]
+
+        self.flag_msg.data = self.at_goal_flag
+
+    def odom_cb(self, msg):
+        self.x_pos = msg.pose.pose.position.x
+        self.y_pos = msg.pose.pose.position.y
+        x = msg.pose.pose.orientation.x
+        y = msg.pose.pose.orientation.y
+        z = msg.pose.pose.orientation.z
+        w = msg.pose.pose.orientation.w
+        _ , _ , self.theta_robot = tf.euler_from_quaternion([x , y , z ,w])
+
+    def front_clear_cb(self, msg):
+        self.clear_path = msg.data
+
+    def cleanup(self):  
+        vel_msg = Twist() 
+        self.pub_cmd_vel.publish(vel_msg) 
+
+if __name__ == "__main__":  
+    GoToGoal()
+
