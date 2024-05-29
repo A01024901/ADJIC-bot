e0f533adb3ab73ba95e97a618dd4fc6331a560b6
A01024901 <A01024901@itesm.mx>
2024-05-29 12:06:31 -0600

    First iteration aruco_finder

--- Minichallenges/minichal7/src/vision/aruco_finder.py
+++ Minichallenges/minichal7/src/vision/aruco_finder.py
@@ -10,24 +10,53 @@ class aruco:
         self.x = x
         self.y = y
 
-        self.camera_t_robot = np.array([
+        self.robot2camera = np.array([
             [0.0, 0.0, 1.0, 0.1],
             [1.0, 0.0, 0.0, 0.0],
             [0.0, -1.0, 0.0, 0.065],
             [0.0, 0.0, 0.0, 1.0]])
         
-        self.aruco2origin = np.array([
+        self.origin2aruco = np.array([
             [1, 0.0, 0, x],
             [0, 1, 0.0, y],
             [0.0, 0, 1, 0.1],
             [0.0, 0.0, 0.0, 1.0]])
         
-    def get_trasnform():
-        pass
+    def get_trasnform(self , x , y  , z):
+        camera2aruco = np.array([
+            [1, 0.0, 0, x],
+            [0, 1, 0.0, y],
+            [0.0, 0, 1, z],
+            [0.0, 0.0, 0.0, 1.0]])
+        
+        aruco2camera = self.get_inv(camera2aruco)
+        camera2robot = self.get_inv(self.robot2camera)
+        
+        self.origin2robot = self.origin2aruco.dot(aruco2camera).dot(camera2robot)
 
+        return self.origin2robot
+
+    def get_inv(self , T):
+        R = T[:3, :3]
+        t = T[:3, 3]
+        
+        # Calcular la inversa de la matriz de rotación (la transpuesta)
+        R_inv = R.T
+        
+        # Calcular la nueva traslación
+        t_inv = -R_inv @ t
+        
+        # Construir la matriz homogénea inversa
+        T_inv = np.eye(4)
+        T_inv[:3, :3] = R_inv
+        T_inv[:3, 3] = t_inv
+        
+        return T_inv
+    
+    
 
 class ArucoFinder:
-    def __init__(self):
+    def __init__(self , mode):
         ###--- Inicio del Nodo ---###
         rospy.init_node('aruco_finder')
         rospy.on_shutdown(self.cleanup)
@@ -44,47 +73,46 @@ class ArucoFinder:
             [0.0, 0.0, 0.0, 1.0]])
 
         ###--- Objetos ---####
-        self.arucos = [aruco(702 , 0.0 , 0.80) , aruco(701 , 0.0 , 1.60) , aruco(703 , 1.73 , 0.80) , 
+        arucos = [aruco(702 , 0.0 , 0.80) , aruco(701 , 0.0 , 1.60) , aruco(703 , 1.73 , 0.80) , 
                        aruco(704 , 2.63 , 0.39) , aruco(705 , 2.85 , 0.0) , aruco(706 , 2.87 , 1.22)]
         
-        self.arucos_sim = [aruco(702 , 0.0 , 0.80) , aruco(701 , 0.0 , 1.60) , aruco(703 , 1.73 , 0.80) , 
+        arucos_sim = [aruco(702 , 0.0 , 0.80) , aruco(701 , 0.0 , 1.60) , aruco(703 , 1.73 , 0.80) , 
                        aruco(704 , 2.63 , 0.39) , aruco(705 , 2.85 , 0.0) , aruco(706 , 2.87 , 1.22)]
+        
+        if mode == "sim": self.arucos = arucos_sim
+        elif mode == "real": self.arucos = arucos
         
         self.fiducial_transform = FiducialTransformArray()
         rate = rospy.Rate(int(1.0 / self.dt))
-
-        while rospy.get_time() == 0: pass 
 
         while not rospy.is_shutdown():
             self.process_transforms()
             rate.sleep()
 
     def process_transforms(self):
-        for aruco in self.fiducial_transform.transforms:
-            if aruco.fiducial_id == self.id:
-                print("Fiducial ID: ", aruco.fiducial_id)
+        if self.fiducial_transform.transforms:
+            pub_msg = np.eye(4)
+            for arucos in self.fiducial_transform.transforms:
+                for posiciones in self.arucos:
+                    if arucos.fiducial_id == posiciones.ID:
+                        print("Fiducial ID: ", posiciones.ID)
 
-                cam_p_aruco = np.array([
-                    aruco.transform.translation.x, 
-                    aruco.transform.translation.y, 
-                    aruco.transform.translation.z
-                ])
+                        x = arucos.transform.translation.x, 
+                        y = arucos.transform.translation.y, 
+                        z = arucos.transform.translation.z
+                        
+                        origin2robot = posiciones.get_trasnform(x , y , z)
+                        distance = self.get_distance(origin2robot)
 
-                robot_p_aruco = self.transform_marker_position(cam_p_aruco)
+                        if self.get_distance(pub_msg) > distance:
+                            pub_msg = origin2robot
 
-                distance = np.linalg.norm(robot_p_aruco)
-                angle_rad = np.arctan2(robot_p_aruco[1], robot_p_aruco[0])
-                angle_deg = np.degrees(angle_rad)
+                    
 
-                print("Distance to marker: %f", distance)
-                print("Angle to marker: %f degrees", angle_deg)
-
-    def transform_marker_position(self, cam_p_aruco):
-        # Transform to robot frame
-        cam_p_aruco_homogeneous = np.append(cam_p_aruco, 1.0)  # Convert to homogeneous coordinates
-        robot_p_aruco_homogeneous = np.dot(self.camera_t_robot, cam_p_aruco_homogeneous)
-        robot_p_aruco = robot_p_aruco_homogeneous[:3]  # Convert back to Cartesian coordinates
-        return robot_p_aruco
+    def get_distance(self , m):
+        d = m[:3 , 3]
+        distance = np.linalg.norm(d)
+        return distance
     
     def ft_cb(self, msg):
         self.fiducial_transform = msg
